2b0683982c4736740f0d69b9a4294f9b6b22f1c1
Óscar Valverde Escobar <ove1001@alu.ubu.es>
2022-11-30 19:16:33 +0100

    cambio oscar prueba datos

--- VYP_OscaryMarcos/UnitTest1_Oscar.cs
+++ VYP_OscaryMarcos/UnitTest1_Oscar.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace VYP_OscaryMarcos
 {
     [TestClass]
@@ -6,21 +8,38 @@ namespace VYP_OscaryMarcos
         [TestInitialize]
         public void Init()
         {
-            Validador validador = new Validador();
+            //Validador validador = new Validador();
         }
 
         [TestMethod]
         public void ValidacióCódigoPostal()
         {
             String codigoPostal = "09007";
-            Assert.IsTrue(validador.EsCodigoPostal(codigoPostal));
+            //Assert.IsTrue(validador.EsCodigoPostal(codigoPostal));
+
+            /**************************************************************************
+             * 
+             * Los dos primeros digitos del codigo postal tienen que ir desde 01 hasta 52
+             * Los codigos postales tienen que tener 5 digitos 
+             * Existen 3 excepciones que son 070, 071 y 080 que son codigos postales de 
+             * 
+             **************************************************************************/
 
         }
 
         [TestMethod]
-        public void ValidaciónCorreoElectrónico()
-        {
+        [DataTestMethod]
+        [DataRow("asd.fgh.es", -1, DisplayName = "EMail Incorrecto sin @")]
+        [DataRow("", -1, DisplayName = "EMail longitud 0")]
+        [DataRow("@asdfgh.es", -1, DisplayName = "EMail Incorrecto sin usuario")]
+        [DataRow("asd.asd@fgh.es", 1, DisplayName = "EMail Correcto")]
+        [DataRow("asd@fgh.es", 1, DisplayName = "EMail Correcto")]
 
+        public void ValidaciónCorreoElectrónico(string eMail, int previsto)
+        {
+            EmailAddressAttribute u1 = new EmailAddressAttribute();
+            if (previsto > 0) Assert.IsTrue(u1.IsValid(eMail));
+            else Assert.IsFalse(u1.IsValid(eMail));
         }
 
         [TestMethod]
